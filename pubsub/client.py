5276b474896e4664de533ff06a479ae6cd844f43
Tero Marttila <terom@fixme.fi>
2014-02-09 18:11:19 +0200

    pubsub.client: implement query/subscribe behaviour

--- pubsub/client.py
+++ pubsub/client.py
@@ -3,11 +3,13 @@
     Publish-Subscribe client.
 """
 
+
 import pubsub.jsonish
 import pubsub.protocol
 
 from pubsub.protocol import Message
 
+import collections
 import logging; log = logging.getLogger('pubsub.client')
 
 class Client :
@@ -15,27 +17,90 @@ class Client :
         self.server = pubsub.protocol.Transport.connect(server_ip, server_port)
         log.info("Connected to server on %s", self.server)
 
-    def subscribe (self, sensors) :
+        self.sendseq = collections.defaultdict(int)
+
+    def send (self, type, payload=None, seq=None, **opts) :
         """
-            Send a subscribe message to the server.
+            Build a Message and send it to the server.
         """
 
-        msg = Message(Message.SUBSCRIBE, payload=list(sensors))
+        if seq is None and payload :
+            # stateful query auto-sendseq
+            seq = self.sendseq[Message.SUBSCRIBE] + 1
+            self.sendseq[Message.SUBSCRIBE] = seq
 
-        log.info("%s", msg)
-        
+        elif not seq :
+            # stateless query
+            seq = 0
+
+        msg = Message(type, payload=payload, seq=seq, **opts)
+
+        log.debug("%s", msg)
+
         self.server(msg)
 
-    def publish (self, msg) :
+    def send_subscribe (self, sensors=None) :
+        """
+            Send a subscribe query/request to the server.
+        """
+
+        log.info("%s", sensors)
+
+        if sensors is True :
+            # subscribe-request: all
+            self.send(Message.SUBSCRIBE, True)
+
+        elif sensors :
+            # subscribe-request: [sensor]
+            self.send(Message.SUBSCRIBE, list(sensors))
+
+        elif not sensors :
+            # subscribe-query
+            self.send(Message.SUBSCRIBE, seq=False)
+
+        else :
+            raise ValueError(sensors)
+
+    def recv_subscribe (self, seq, sensors) :
+        """
+            Process a subscribe-response/update from server.
+        """
+        
+        log.info("%s", sensors)
+
+        return sensors
+
+    def recv_publish (self, seq, update) :
         """
             Process a publish from the server.
         """
 
-        sensors = msg.payload
+        log.info("%s", update)
+        
+        return { update['dev_id']: update['sensor_data'] }
+    
+    RECV = {
+            Message.SUBSCRIBE:  recv_subscribe,
+            Message.PUBLISH:    recv_publish,
+    }
 
-        log.info("%s", sensors)
+    def recv (self, msg) :
+        """
+            Handle recevied message.
+        """
 
-        return sensors
+        log.debug("%s", msg)
+
+        # TODO: acks
+
+
+        if msg.type in self.RECV :
+            ret = self.RECV[msg.type](self, msg.seq, msg.payload)
+            
+            log.info("%s:%d:%s = %s", msg.type_str, msg.seq, msg.payload, ret)
+
+        else :
+            log.warning("Received unknown message type from server: %s", msg)
 
     def __iter__ (self) :
         """
@@ -43,9 +108,40 @@ class Client :
         """
 
         for msg, addr in self.server :
-            # process
-            if msg.type == Message.PUBLISH :
-                yield self.publish(msg)
+            # XXX: check addr matches server addr
+            
+            out = self.recv(msg)
+
+            if out is not None :
+                yield msg.type, out
+
+    def query (self) :
+        """
+            Send a query request, and wait for response.
+        """
+
+        self.send_subscribe()
+
+        for type, msg in self :
+            if type == Message.SUBSCRIBE :
+                return msg
 
             else :
-                log.warning("Received unknown message type from server: %s", msg)
+                log.warning("unknown response to subscribe-query: %d:%s", type, msg)
+
+    def subscribe (self, sensors=True) :
+        """
+            Setup a subscription, and yield sensor publishes.
+        """
+
+        self.send_subscribe(sensors)
+
+        for type, msg in self :
+            if type == Message.PUBLISH :
+                yield msg
+
+            else :
+                log.warning("unhandled response to subscribe-request: %d:%s", type, msg)
+
+    def __str__ (self) :
+        return str(self.server)

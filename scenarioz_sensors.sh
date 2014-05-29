a9ce41eaadea6dbee3b9ed750d98a5ee77d92e63
Tero Paloheimo <tero.paloheimo@iki.fi>
2014-05-29 13:41:13 +0300

    added scenario Z sensor start-up script

---
+++ scenarioz_sensors.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+./sensor.py camera 127.0.0.1 1336 1 &
+# ./sensor.py camera 127.0.0.1 1336 2 &
+# ./sensor.py camera 127.0.0.1 1336 3 &
+# ./sensor.py camera 127.0.0.1 1336 4 &
+# ./sensor.py camera 127.0.0.1 1336 5 &
+
+./sensor.py device 127.0.0.1 1336 1 &
+./sensor.py device 127.0.0.1 1336 2 &
+./sensor.py device 127.0.0.1 1336 3 &
+./sensor.py device 127.0.0.1 1336 4 &
+./sensor.py device 127.0.0.1 1336 5 &
+./sensor.py device 127.0.0.1 1336 6 &
+./sensor.py device 127.0.0.1 1336 7 &
+./sensor.py device 127.0.0.1 1336 8 &
+./sensor.py device 127.0.0.1 1336 9 &
+./sensor.py device 127.0.0.1 1336 10 &
+./sensor.py device 127.0.0.1 1336 11 &
+./sensor.py device 127.0.0.1 1336 12 &
+./sensor.py device 127.0.0.1 1336 13 &
+./sensor.py device 127.0.0.1 1336 14 &
+./sensor.py device 127.0.0.1 1336 15 &
+./sensor.py device 127.0.0.1 1336 16 &
+./sensor.py device 127.0.0.1 1336 17 &
+./sensor.py device 127.0.0.1 1336 18 &
+./sensor.py device 127.0.0.1 1336 19 &
+./sensor.py device 127.0.0.1 1336 20 &
+
+./sensor.py temp 127.0.0.1 1336 1 &
+./sensor.py temp 127.0.0.1 1336 2 &
+./sensor.py temp 127.0.0.1 1336 3 &
+./sensor.py temp 127.0.0.1 1336 4 &
+./sensor.py temp 127.0.0.1 1336 5 &
+
+./sensor.py gps 127.0.0.1 1336 1 &
+./sensor.py gps 127.0.0.1 1336 2 &
+
+./sensor.py asd 127.0.0.1 1336 1 &
+# ./sensor.py asd 127.0.0.1 1336 2 &
+# ./sensor.py asd 127.0.0.1 1336 3 &
+# ./sensor.py asd 127.0.0.1 1336 4 &
+# ./sensor.py asd 127.0.0.1 1336 5 &
+# ./sensor.py asd 127.0.0.1 1336 6 &
+# ./sensor.py asd 127.0.0.1 1336 7 &

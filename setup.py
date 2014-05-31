36c848dbc1af7d05269d8a325233db2210a47410
Tero Marttila <terom@fixme.fi>
2014-05-31 23:57:55 +0300

    add version 1.0 setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from pubsub import __version__
+
+
+setup(
+    name            = 'pubsub',
+    version         = __version__,
+    description     = 'Publish-Subscribe protocol implementation',
+
+    packages        = [
+        'pubsub',
+    ],
+    scripts         = [
+        'ps-client',
+        'ps-server',
+    ],
+)
